2c8952085d74fb1b692083a332a8d64e0b42f4ca
SimpleLifeSimpleLove <1137247975@qq.com>
2020-12-22 22:04:17 +0800

    AcWing 0001 A + B

---
+++ AcWingQuestions/README.md
@@ -0,0 +1,7 @@
+# AcWing在线题库
+
+https://www.acwing.com/problem/
+
+1. A + B
+
+
---
+++ AcWingQuestions/src/_0001/Main.java
@@ -0,0 +1,21 @@
+package _0001;
+
+import java.util.Scanner;
+
+/**
+ * Date: 2020/12/22 21:59
+ * Content:
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a, b;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println(a + b);
+
+        scanner.close();
+    }
+}
