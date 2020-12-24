3aef0d86d0dbbbd1fc1c216217ea2c922c7e6940
SimpleLifeSimpleLove <1137247975@qq.com>
2020-12-24 15:55:25 +0800

    快排模板

--- Algorithm-01-BasicCourse-2/src/_01_BasicAlgorithm/_01_sort/quicksort/Main.java
+++ Algorithm-01-BasicCourse-2/src/_01_BasicAlgorithm/_01_sort/quicksort/Main.java
@@ -30,6 +30,21 @@ public class Main {
         quickSort(arr, j + 1, r);
     }
 
+    private static void quickSort2(int[] arr, int l, int r) {
+
+        if (l >= r) return;
+
+        int x = arr[(l + r + 1) >> 1], i = l - 1, j = r + 1;
+        while (i < j) {  // 双指针
+            do i++; while (arr[i] < x);
+            do j--; while (arr[j] > x);
+            if (i < j) swap(arr, i, j);
+        }
+
+        quickSort2(arr, l, i - 1);
+        quickSort2(arr, i, r);
+    }
+
     public static void main(String[] args) throws Exception {
 
         // 读入数据
