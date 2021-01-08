cf17c1410b0973a81fb7524ea629d3d4c93ecb8f
SimpleLifeSimpleLove <1137247975@qq.com>
2021-01-08 14:39:03 +0800

    AcWing 0867 分解质因数

---
+++ Cplusplus/AcWingQuestions/_0867/_867_main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void divide(int x) {
+    for (int i = 2; i <= x / i; i++)
+        if (x % i == 0) {
+            int s = 0;  // 记录该质因子的个数
+            while (x % i == 0) x /= i, s++;
+            printf("%d %d\n", i, s);
+        }
+    if (x > 1) printf("%d 1\n", x);
+}
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    while (n--) {
+        int x;
+        scanf("%d", &x);
+        divide(x);
+        printf("\n");
+    }
+
+    return 0;
+}
