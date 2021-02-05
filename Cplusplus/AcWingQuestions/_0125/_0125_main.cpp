e64a01b3f263fae4ec529bc197e05aa4afcf50f8
‘ToLoveToFeel’ <1137247975@qq.com>
2021-02-05 14:25:26 +0800

    AcWing 0125 耍杂技的牛

---
+++ Cplusplus/AcWingQuestions/_0125/_0125_main.cpp
@@ -0,0 +1,36 @@
+// Created by WXX on 2021/2/5 14:10
+#include <iostream>
+#include <algorithm>
+
+#define x first
+#define y second
+
+using namespace std;
+
+typedef pair<int, int> PII;
+const int N = 50010;
+
+int n;
+PII a[N];
+
+int main() {
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        int w, s;
+        scanf("%d%d", &w, &s);
+        a[i] = {w + s, w};
+    }
+
+    sort(a, a + n);
+    int res = -1e9;
+    for (int i = 0, sum = 0; i < n; i++) {
+        int w = a[i].y, s = a[i].x - w;
+        res = max(res, sum - s);
+        sum += w;
+    }
+
+    printf("%d\n", res);
+
+    return 0;
+}
